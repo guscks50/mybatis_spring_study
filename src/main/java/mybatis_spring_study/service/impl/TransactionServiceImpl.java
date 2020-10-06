b1b35e33336167b19ec2afdf10bdfd721cc988b0
hyunchan <guscks50@naver.com>
2020-10-06 10:28:09 +0900

    에노테이션을 이용한 트렌젝션

--- src/main/java/mybatis_spring_study/service/impl/TransactionServiceImpl.java
+++ src/main/java/mybatis_spring_study/service/impl/TransactionServiceImpl.java
@@ -1,5 +1,7 @@
 package mybatis_spring_study.service.impl;
 
+import javax.management.RuntimeErrorException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,13 +25,24 @@ public class TransactionServiceImpl implements TransactionService {
 		//부서가 등록되고 난후 해당 부서에 사원을 추가
 		deptMapper.insertDepartment(department);
 		empMapper.insertEmployee(employee);
-		
+
+
+		int res = deptMapper.insertDepartment(department);
+		res += empMapper.insertEmployee(employee);
+		if (res != 2) throw new RuntimeException();
+
 
 	}
 
 	@Override
+	@Transactional
 	public void unRegisterTransaction(Department department, Employee employee) {
 		
+		empMapper.deleteEmployee(employee);
+		deptMapper.deleteDepartment(department);
+		int res = empMapper.deleteEmployee(employee);
+		res += deptMapper.deleteDepartment(department);
+		if (res != 2) throw new RuntimeException();
 
 	}
 
--- src/test/java/mybatis_spring_study/mapper/TransactionServiceImplTest.java
+++ src/test/java/mybatis_spring_study/mapper/TransactionServiceImplTest.java
@@ -2,6 +2,7 @@ package mybatis_spring_study.mapper;
 
 import static org.junit.Assert.fail;
 
+import java.sql.SQLException;
 import java.util.DuplicateFormatFlagsException;
 
 import org.apache.ibatis.logging.Log;
@@ -19,45 +20,66 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import mybatis_spring_study.dto.Department;
 import mybatis_spring_study.dto.Employee;
 import mybatis_spring_study.service.TransactionService;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "file:src/main/resources/context-root.xml" })
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TransactionServiceImplTest {
 	private static final Log log = LogFactory.getLog(TransactionServiceImplTest.class);
 
-	
 	@After
 	public void tearDown() throws Exception {
-	System.out.println();
+		System.out.println();
 	}
+
 	@Autowired
 	private TransactionService service;
-	
+
 	@Test(expected = DuplicateFormatFlagsException.class)
 	public void testARegisterTransaction_Dept_Fail() {
 		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
 		Department department = new Department(1, "태스크포스", 10); // DuplicateKeyException
 		Employee employee = new Employee(1004, "박신혜", "과장", new Employee(4377), 4100000, department);
-		
+
 		service.registerTransaction(department, employee);
 	}
+
 	@Test(expected = DuplicateKeyException.class)
 	public void testBRegisterTransaction_Emp_Fail() {
 		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
-		Department department = new Department(6, "태스크포스", 10); 
+		Department department = new Department(6, "태스크포스", 10);
 		Employee employee = new Employee(1005, "박신혜", "과장", new Employee(4377), 4100000, department);
-		
+
 		service.registerTransaction(department, employee);
 
 	}
-	@Test
-	public void testCRegisterTransaction_Success() {
-		fail("Not yet implemented");
+
+	@Test(expected = RuntimeException.class)
+	public void testDCRegisterTransaction_Fail_Department() {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		Department department = new Department(100); // RuntimeException -> rollback
+		Employee employee = new Employee(1005); // rollback 되므로 삭제되면 안됨
+
+		service.unRegisterTransaction(department, employee);
+
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void testEUnregisterTransaction_Fail_Emp() throws SQLException {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		Department department = new Department(6);// 정상삭제
+		Employee employee = new Employee(9999);// RuntimeException -> rollback
+		
+		service.unRegisterTransaction(department, employee);
 	}
 
 	@Test
-	public void testUnRegisterTransaction() {
-		fail("Not yet implemented");
+	public void testFUnregisterTransaction_Success() throws SQLException {
+		log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + "()");
+		Department department = new Department(6);
+		Employee employee = new Employee(1005);
+		
+		service.unRegisterTransaction(department, employee);
 	}
 
 }
